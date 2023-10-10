6ac4569b4891b7e0274291149807ea31797e5bc9
Will Roberts <willroberts3@protonmail.com>
2023-10-10 17:40:34 -0400

    Adds gRPC listener to Spring application

--- src/main/java/io/github/willroberts/springgrpcservice/SpringGrpcServiceApplication.java
+++ src/main/java/io/github/willroberts/springgrpcservice/SpringGrpcServiceApplication.java
@@ -1,13 +1,35 @@
 package io.github.willroberts.springgrpcservice;
 
+import java.io.IOException;
+
+import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+
 @SpringBootApplication
 public class SpringGrpcServiceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SpringGrpcServiceApplication.class, args);
-	}
 
+		Server server = ServerBuilder
+				.forPort(8000)
+				.addService(new HelloServiceImpl()).build();
+
+		try {
+			System.out.println("Starting gRPC server on localhost:8000");
+			server.start();
+		} catch (Exception e) {
+			System.out.println("Failed to start gRPC server:" + e);
+		}
+
+		try {
+			System.out.println("Listening for requests");
+			server.awaitTermination();
+		} catch (Exception e) {
+			System.out.println("Error during gRPC server operation:" + e);
+		}
+	}
 }
