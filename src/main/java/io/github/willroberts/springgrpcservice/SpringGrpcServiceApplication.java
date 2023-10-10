c2003f979646d8497cf67bdf33fca3295f88d6da
Will Roberts <willroberts3@protonmail.com>
2023-10-10 17:00:52 -0400

    Adds initial project files

---
+++ src/main/java/io/github/willroberts/springgrpcservice/SpringGrpcServiceApplication.java
@@ -0,0 +1,13 @@
+package io.github.willroberts.springgrpcservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGrpcServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGrpcServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/willroberts/springgrpcservice/SpringGrpcServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.willroberts.springgrpcservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGrpcServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
